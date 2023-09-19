3a14a4ff93108291f16daa44f95aa2d45bca667c
Daniellllracz <78079867+Daniellllracz@users.noreply.github.com>
2023-09-19 08:34:49 +0200

    Add files via upload

---
+++ UltraBalaton/UltraBalaton/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace UltraBalaton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var versenyzok = new List<Versenyzo>();
+            using var sr = new StreamReader(
+                path: "../../../src/ub2017egyeni.txt",
+                encoding: Encoding.UTF8);
+            _ = sr.ReadLine();
+
+            while (!sr.EndOfStream) versenyzok.Add(new Versenyzo(sr.ReadLine()));
+
+            Console.WriteLine($"3.Feladat: Egyéni indulók: {versenyzok.Count} fő");
+            var f4 = versenyzok.Count(v => !v.Kategoria && v.TeljesTav == 100);
+            Console.WriteLine($"4. feladat: teljes tavot teljesito nok {f4} fo");
+
+        }   
+    }
+}
---
+++ UltraBalaton/UltraBalaton/Versenyzo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltraBalaton
+{
+    class Versenyzo
+    {
+        public string Nev { get; set; }
+        public int VersenySzam { get; set; }
+        public bool Kategoria { get; set; }
+        public TimeSpan IdoPontszamok { get; set; }
+        public int TeljesTav { get; set; }
+
+        public Versenyzo(string beolvasottSor)
+        {
+            string[] adat = beolvasottSor.Split(';');
+            this.Nev = adat[0];
+            this.VersenySzam = int.Parse(adat[1]);
+            this.Kategoria = adat[2] == "Ferfi";
+            var ido = adat[3].Split(";");
+            IdoPontszamok = new TimeSpan(
+               hours: int.Parse(ido[0]),
+               minutes: int.Parse(ido[1]),
+               seconds: int.Parse(ido[2]));
+            this.TeljesTav = int.Parse(adat[4]);
+        }
+    }
+}
